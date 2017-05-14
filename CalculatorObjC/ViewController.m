97c8d9d159dad3c9be2100a2bbd5f7a6ec1d3799
Oleg Myatlikov VM <olegmyatlikov@gmail.com>
2017-05-14 23:41:19 +0300

    [JIRA-1] Add number's digit and display

--- CalculatorObjC/ViewController.m
+++ CalculatorObjC/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (retain, nonatomic) IBOutlet UILabel *display;
 
 @end
 
@@ -16,14 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)clearButton:(UIButton *)sender {
+    _display.text = @"0";
 }
 
 
+- (IBAction)digitButton:(UIButton *)sender {
+    
+    /*NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    formatter.maximumFractionDigits = 20;
+    NSString *number = [formatter stringFromNumber:@1.];
+    NSNumber* number = [[[NSNumber alloc] init] autorelease];
+    number = [NSString stringWithFormat:@"%f", [result doubleValue]];
+    */
+    
+    NSString* result = [[[NSString alloc] init] autorelease];
+    result = [NSString stringWithFormat:@"%@", _display.text];
+    result = [result stringByAppendingString: sender.titleLabel.text];
+    _display.text = [NSString stringWithFormat: @"%ld", (long)[result integerValue]];
+    //NSLog(@"%f", [formatter stringFromNumber: number]);
+}
+
+
+
+
+- (void)dealloc {
+    [_display release];
+    [super dealloc];
+}
 @end
