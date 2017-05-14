f1cc7e7a1358fb12daab2e8f641514ee88261bae
Admin <admin@Mac-Admin.local>
2017-05-14 11:52:36 +0300

    Initial Commit

---
+++ CalculatorObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorObjC
+//
+//  Created by Admin on 14.05.17.
+//  Copyright © 2017 Oleg Myatlikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalculatorObjC
+//
+//  Created by Admin on 14.05.17.
+//  Copyright © 2017 Oleg Myatlikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
