7c9b2f9d8af3c7535229be0461597244a596eb8c
Admin <admin@Mac-Admin.local>
2017-05-15 11:59:22 +0300

    [JIRA-1] Add "dot" button and swipe to delete

--- CalculatorObjC/ViewController.m
+++ CalculatorObjC/ViewController.m
@@ -9,40 +9,63 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+
 @property (retain, nonatomic) IBOutlet UILabel *display;
+@property (assign, nonatomic) BOOL userMiddleOfTyping;
 
 @end
 
+
 @implementation ViewController
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UISwipeGestureRecognizer *swipeDellLastNumOnDisplay = [[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeOnDisplay:)] autorelease];
+    swipeDellLastNumOnDisplay.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.display addGestureRecognizer:swipeDellLastNumOnDisplay];
 }
 
 
 - (IBAction)clearButton:(UIButton *)sender {
     _display.text = @"0";
+    _userMiddleOfTyping = NO;
+}
+
+
+- (IBAction)dotButton:(UIButton *)sender {
+    if (![_display.text containsString:@"."]) {
+        _display.text = [NSString stringWithFormat:@"%@.", _display.text];
+        _userMiddleOfTyping = YES;
+    }
 }
 
 
 - (IBAction)digitButton:(UIButton *)sender {
-    
-    /*NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-    formatter.numberStyle = NSNumberFormatterDecimalStyle;
-    formatter.maximumFractionDigits = 20;
-    NSString *number = [formatter stringFromNumber:@1.];
-    NSNumber* number = [[[NSNumber alloc] init] autorelease];
-    number = [NSString stringWithFormat:@"%f", [result doubleValue]];
-    */
-    
-    NSString* result = [[[NSString alloc] init] autorelease];
-    result = [NSString stringWithFormat:@"%@", _display.text];
-    result = [result stringByAppendingString: sender.titleLabel.text];
-    _display.text = [NSString stringWithFormat: @"%ld", (long)[result integerValue]];
-    //NSLog(@"%f", [formatter stringFromNumber: number]);
+    NSString *result = [[[NSString alloc] init] autorelease];
+    if (_userMiddleOfTyping) {
+        result = [NSString stringWithFormat:@"%@", _display.text];
+        result = [result stringByAppendingString: sender.titleLabel.text];
+    } else {
+        result = [NSString stringWithFormat:@"%@", sender.titleLabel.text];
+    }
+    _display.text = [NSString stringWithFormat: @"%@", result];
+    _userMiddleOfTyping = YES;
 }
 
+
+- (void)swipeOnDisplay:(UIGestureRecognizerState *)recognizer {
+    NSString *result = [[[NSString alloc] init] autorelease];
+    result = [NSString stringWithFormat:@"%@", _display.text];
+    if ([result length] > 1) {
+        result = [result substringToIndex:[result length] - 1];
+    } else {
+        result = @"0";
+        _userMiddleOfTyping = NO;
+    }
+    _display.text = result;
+}
 
 
 
@@ -50,4 +73,5 @@
     [_display release];
     [super dealloc];
 }
+
 @end
