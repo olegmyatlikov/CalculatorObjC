e95b3c340d0026ad046e5b9c128e754426ee0b6e
Oleg Myatlikov (Work VM) <olegmyatlikov@gmail.com>
2017-05-18 15:33:21 +0300

    [JIRA-1.5] Add the "about us" page in stack

---
+++ CalculatorObjC/AboutUsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutUsViewController.h
+//  CalculatorObjC
+//
+//  Created by Admin on 18.05.17.
+//  Copyright © 2017 Oleg Myatlikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutUsViewController : UIViewController
+
+@end
