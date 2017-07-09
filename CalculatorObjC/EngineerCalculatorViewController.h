a98b4db4109b488c893821fbca66729c9a25303d
Oleg Myatlikov <olegmyatlikov@gmail.com>
2017-07-09 22:47:03 +0300

    [JIRA-4.3] Add engineer calculator

---
+++ CalculatorObjC/EngineerCalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  EngineerCalculatorViewController.h
+//  CalculatorObjC
+//
+//  Created by Oleg Myatlikov on 7/9/17.
+//  Copyright © 2017 Oleg Myatlikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EngineerCalculatorViewController : UIViewController
+
+@end
